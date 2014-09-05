8129ee6f5f228cf8fd1db985c956482519549f3f
OlegTsyba <idethrone1@gmail.com>
2014-09-06 01:43:52 +0300

    add checklist method

--- lib/contract_programming.rb
+++ lib/contract_programming.rb
@@ -3,10 +3,13 @@ require "contract_programming/version"
 module ContractProgramming
   def contract(method)
     prepend( Module.new do
-        private def check_it(msg, rez)
+        private def checklist
           @checklist ||= []
-          @checklist << msg unless rez
-          @checklist
+        end
+
+        private def check_it(msg, rez)
+          checklist << msg unless rez
+          checklist
         end
 
         define_method method do |*args, &bl|
@@ -14,14 +17,14 @@ module ContractProgramming
 
           if respond_to? pre, true
             send pre, *args, &bl 
-            @checklist.each{ |msg| raise ArgumentError, msg }.clear
+            checklist.each{ |msg| raise ArgumentError, msg }.clear
           end
 
           result = super *args, &bl
 
           if respond_to? post, true
             send post, result, &bl
-            @checklist.each{ |msg| raise ArgumentError, msg }.clear
+            checklist.each{ |msg| raise ArgumentError, msg }.clear
           end
 
           result
